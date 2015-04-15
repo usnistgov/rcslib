7bf6a9351df66a15eb978abcf2b431383f3db433
Will Shackleford <william.shackleford@nist.gov>
2015-04-15 11:27:07 -0400

    add script so logs are visible in jenkins log

---
+++ etc/cat_tmp_logs.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for ldir in /tmp/tni_* ; do
+    echo ""
+    echo "Start Log Directory: ${ldir}"
+    echo ""
+    ls -l "${ldir}"
+    for log in "${ldir}/*.log" ; do
+        echo ""
+        echo "Start Log: ${log}"
+        echo ""
+        cat "${log}"
+        echo ""
+        echo "End Log: ${log}"
+        echo ""
+    done;
+    echo ""
+    echo "End Log Directory: ${ldir}"
+    echo ""
+done;
