bb7825e73a1ccdd1923bef224ac64b85fa9301a2
Will Shackleford <william.shackleford@nist.gov>
2015-04-15 15:23:55 -0400

    changes needed for (GNU Autoconf) 2.69

--- etc/cat_tmp_logs.bash
+++ etc/cat_tmp_logs.bash
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-set -x;
+#set -x;
+ldir=`pwd`;
+
+echo ""
+echo "Start Log Directory: ${ldir}"
+echo ""
+ls -l "${ldir}"
+for log in "${ldir}/"*.log ; do
+    if test "x${log}" != "x" -a -f "${log}" ; then
+        echo ""
+        echo "Start Log: ${log}"
+        echo ""
+        cat "${log}"
+        echo ""
+        echo "End Log: ${log}"
+        echo ""
+    fi
+ done;
+echo ""
+echo "End Log Directory: ${ldir}"
+echo ""
 
 for ldir in /tmp/tni_* ; do
     echo ""
