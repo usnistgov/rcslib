81a415e9f78a3494ba432d7a207cc3ebfd04063c
Will Shackleford <william.shackleford@nist.gov>
2015-05-04 10:56:02 -0400

    set new_version = 2015.05.04

--- src/java/rcs/RCS_VERSION.java
+++ src/java/rcs/RCS_VERSION.java
@@ -60,13 +60,13 @@ public class RCS_VERSION extends Object
         /**
         * String in which the library was compiled is stored.
         */
-        public static final String date_string =  "Mon May  4 10:54:09 EDT 2015";
+        public static final String date_string =  "Mon May  4 10:54:49 EDT 2015";
 
         /**
         * String in which a brief message describing the library, it's version
         * and compile date etc. is stored.
         */
-        public static final String info_string = "@(#)" + " RCS_LIBRARY_VERSION: 2015.05.04 Compiled on "+ "Mon May  4 10:54:09 EDT 2015" +" for the java platform.\n";
+        public static final String info_string = "@(#)" + " RCS_LIBRARY_VERSION: 2015.05.04 Compiled on "+ "Mon May  4 10:54:49 EDT 2015" +" for the java platform.\n";
 
         /**
         * Function that returns the info_string.
